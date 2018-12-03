d438f8917e13e5e0fb66a7c319554696c327fc95
Samuel Falci <samuel@in8.com.br>
2018-12-03 13:35:12 -0200

    alterações no cadastro e alterações de turmas

---
+++ api/config.php
@@ -0,0 +1,11 @@
+<?php
+namespace SistemaIfnmg;
+$conn = array(
+    'driver' => 'pdo_mysql',
+    'host' => 'localhost',
+    'dbname' => 'sistemaifnmg',
+    'user' => 'root',
+    'password' => 'BuRox2005',
+    'charset' => 'utf-8'
+);
+define("CONEXAO", $conn);
